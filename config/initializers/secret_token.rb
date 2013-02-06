a1258954bafc0dc7f84191e7062cc5802ed9219b
Graham Thomson <graham.thomson@gmail.com>
2013-02-06 16:22:01 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WrittengamesRails::Application.config.secret_token = 'ed09a2f253171a61f6877fff64ba5ff980578517f393912061c67050b79633acca45e74b89f5d43582f6e0848291ba812e1852c78630868d2fe331bd7bf720fc'
